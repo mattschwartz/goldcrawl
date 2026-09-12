0e9308ca712e4de8b6b7047697eb5056d97f4711
Matt Schwartz <mattschwartz@users.noreply.github.com>
2026-09-12 06:49:47 -0500

    set up SDL dependency with VS

---
+++ GoldCrawl/GoldCrawl.cpp
@@ -0,0 +1,9 @@
+#include "Prelude.h"
+#include <iostream>
+
+// fun fact: SDL requires this exact header
+int main(int argc, char* args[])
+{
+    std::cout << "Hello World!\n";
+    return 0;
+}
---
+++ GoldCrawl/Prelude.h
@@ -0,0 +1,9 @@
+#pragma once
+
+#include <SDL.h>
+#include <memory>
+#include <vector>
+#include <unordered_map>
+#include <string>
+#include <optional>
+
